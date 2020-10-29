f4eef8a6d84c0d02718475ec7ae1b55113bfc38c
Revanth12-creator <revanth.sake6@gmail.com>
2020-10-29 10:42:51 +0530

    Fetching github users data using React-js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component ,Fragment } from 'react';
+import axios from "axios";
+import "./App.css";
+import Spinner from "./lodingComponent.jsx";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor(){
+        super()
+        this.state={
+            items:[],
+            Loading:false,
+        }
+    }
+    async componentDidMount(){
+        let githubuser_API= await axios.get("https://api.github.com/users");
+        this.setState({
+            items:githubuser_API.data , Loading:true
+        })
+        console.log(this.state.items)
+    }
+    render() {
+        let {items,Loading}=this.state;
+        let Responce=this.state.items.map((user) =>{
+            return(
+                <Fragment>
+                  
+                    <div className="card"> 
+                        <img src={user.avatar_url} alt={user.login} />
+                        <div className="card-body">
+                            <h1>{user.login}</h1>
+                        </div>
+                    </div>
+                  
+                </Fragment>
+            )
+        })
+        return (
+        <Fragment>
+            <h2 className="title">Github Users</h2>
+           <div className="bodyBlock">
+           {/* <Spinner/> */}
+               {this.state.items ? Responce : "Loading"}
+           </div>
+        </Fragment>      
+        )
+    }
 }
-
 export default App;
---
+++ src/lodingComponent.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import "./loading.css";
+let Spinner=() => {
+    return(
+       <div>
+           <div id="container">
+  <div id="circle-0">
+  </div>
+  <div id="circle-1">
+  </div>
+  <div id="circle-2">
+  </div>
+  <div id="circle-3">
+  </div>
+  <div id="circle-4">
+  </div>
+</div>
+        </div>
+    )
+}
+
+export default Spinner;
